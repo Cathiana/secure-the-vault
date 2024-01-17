b9a598b5bbcb762f8c2e4bd3968eee16e0b54f6a
Cathiana <cathianao@gmail.com>
2024-01-17 17:16:25 -0500

    first commit

---
+++ Pseudocode-GP.js
@@ -0,0 +1,22 @@
+/*Grab a toothbrush with your hand.
+Turn on the water.
+Rinse the brush part of the toothbrush with water.
+Turn off the water.
+Open the tube of toothpaste.
+Squeeze toothpaste on the brush part of the toothbrush.
+Start with the front teeth.
+Move in circular motions with the brush
+*/
+
+let numcows = 50;
+const team = "Hello World";
+console.log("Hello World")
+console.log("Hello, " +  "Disney Land!")
+const umbrellas = 10 * 3 
+let num = 5 + 1 
+console.log (num)
+console.log ("Hello" + " " + "World");
+let sum1 = "Hello"
+let sum2 = "I am"
+let sum3 = "Josh"
+alert (sum1 + " " + sum2 + " " + sum3)
---
+++ Securethevault-GP.js
@@ -0,0 +1,19 @@
+/*Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator. Each must equal one of the three codes in the combination.
+Add comments throughout the code to explain each step in the process.
+Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+Please submit either a file with your code or a screenshot of the code, with the pseudocode showing, or a GitHub link.
+*/
+//always place the console.log below the const & let variables for code to transmit through the HTML
+const unlock = ["You have received this message because you have been chosen to open an important vault. Here is the secret combination:"]
+console.log(unlock)
+//apply arithmetic variables to unlock combination 10
+let lock1 = 5 + 5
+//apply arithmetic variables to unlock combination 40
+let lock2 = 10 * 4
+//apply arithmetic variables to unlock combination 39
+let lock3 = 60 - 21
+console.log(lock1)
+console.log(lock2)
+console.log(lock3)
+
